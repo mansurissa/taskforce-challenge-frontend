cc2b4012b190b2bc5e6a831d56fe04b6fa1ca89c
mansurissa <mansurissa6@gmail.com>
2021-08-20 17:34:46 +0200

    setup tests with jest and github actions

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+export const mockStore = configureMockStore([thunk]);
